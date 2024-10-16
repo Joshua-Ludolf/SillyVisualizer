cab5262563deb38035c6f52fed44a95c7e4746fb
MattjTrev <112988085+MattjTrev@users.noreply.github.com>
2024-10-15 21:54:37 -0500

    Added to app.py Made and added skeleton to silly_visualizer.py Added to Html. Took off file bc lazy will fix later

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,28 @@
 """
-    Group #9
-    Members: Joshua Ludolf, Samantha Jackson, Matthew Trevino, Jonathon Davis
-
-    Class: CSCI 4316 - Software Engineering 1
+Group #9 Members: Joshua Ludolf, Samantha Jackson, Matthew Trevino, Jonathon Davis
+Class: CSCI 4316 - Software Engineering 1
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from silly_visualizer import generate_visualization
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # return render_template('index.html')
-     return render_template('frontend\src\App.js')
+    return render_template('index.html')
+
+@app.route('/visualize', methods=['POST'])
+def visualize():
+    code = request.form['code']
+    language = request.form['language']
+    diagram_type = request.form['diagram_type']
+
+    try:
+        graph_image, title = generate_visualization(code, language, diagram_type)
+        return jsonify({"image": graph_image, "title": title})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=False)
